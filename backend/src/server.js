3f1339dc9d7690eeeb081d6403342980369623d9
Aarushjain2401 <aarushjain2401@gmail.com>
2025-08-13 18:41:09 +0530

    Final Commit

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,29 +1,43 @@
 import express from "express";
-import dotenv from 'dotenv'
+import "dotenv/config";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import path from "path";
+
 import authRoutes from "./routes/auth.route.js";
-import { connectDB } from "./lib/db.js";
 import userRoutes from "./routes/user.route.js";
-import cors from 'cors'
 import chatRoutes from "./routes/chat.route.js";
-import cookieParser from "cookie-parser"
 
-dotenv.config();
+import { connectDB } from "./lib/db.js";
 
-const app = express()
-const PORT = process.env.PORT
+const app = express();
+const PORT = process.env.PORT;
 
-app.use(cors({
+const __dirname = path.resolve();
+
+app.use(
+  cors({
     origin: "http://localhost:5173",
-    credentials: true, //allows frontend to send cookies
-}))
+    credentials: true, // allow frontend to send cookies
+  })
+);
+
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/api/auth",authRoutes);
-app.use("/api/users",userRoutes)
-app.use("/api/chat",chatRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/chat", chatRoutes);
 
-app.listen(PORT,()=>{
-    console.log(`Server is running on port ${PORT}`)
-    connectDB();
-})
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  connectDB();
+});
